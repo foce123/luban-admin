dd5047d0a43d27dbfc623ba0ecb9cdd3704ebadb
zhangtong <zhangtong@lb-dj.com>
2025-03-31 10:24:35 +0800

    update

---
+++ admin-ui/src/views/domain/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="app-container domain">
+        hello domain
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped lang="scss">
+  .domain {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-size: 120px;
+    font-weight: 700;
+    padding-top: 300px;
+  }
+  </style>
+  
